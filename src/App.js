e334552c482a40e2daff3822ed92650de1455808
porosadporosad <tmxk1594@naver.com>
2023-02-06 20:20:20 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element: <Home></Home>,
+  },
+  {
+  path: "/movie/:id",
+  element: <Detail></Detail>,
+  },
+  ]);
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    return <RouterProvider router={router} />;
+  }
+  
 export default App;
